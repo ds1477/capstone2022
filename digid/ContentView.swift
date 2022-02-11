6c22b0af956f2a06ff3b84dd8de309627b04e6ea
Daniel Samojlik <dan.the.man1617@gmail.com>
2022-02-11 13:23:07 -0700

    login page

--- digid/ContentView.swift
+++ digid/ContentView.swift
@@ -12,143 +12,46 @@ struct ContentView: View {
     @State private var netID: String = ""
     @State private var password: String = ""
     @State var color = Color.black.opacity(0.7)
-    @State var visible = false
-    @Binding var show : Bool
-    @State var alert = false
-    @State var error = ""
-    
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Text("digid")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color("deepRed"))
-                Text("A Rutgers Digital ID")
-                        .fontWeight(.light)
-                        .foregroundColor(Color("deepRed"))
-                            
-                TextField("netID", text: self.$netID)
-                    .autocapitalization(.none)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.netID != "" ? Color("deepRed") : Color("deepRed"),lineWidth: 2))
-                    .padding(25)
-                
-                HStack{
-                    VStack {
-                        if self.visible{
-                            TextField("Password", text: self.$password)
-                                .autocapitalization(.none)
-                        }
-                        else {
-                            SecureField("Password", text: self.$password)
-                                .autocapitalization(.none)
-                        }
-                    }
+        NavigationView {
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 150, alignment: .center)
+           
+                VStack {
+                    TextField("netID", text: $netID)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.netID != "" ? Color("Color") : Color("Color"),lineWidth: 2))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : Color("Color"),lineWidth: 2))
+                    
                     Button(action: {
-                        self.visible.toggle()
-                    }) {
-                        Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                            .foregroundColor(self.color)
-                    }
+                        
+                    }, label: {
+                        Text("Sign In")
+                            .foregroundColor(Color.white)
+                            .frame(width:200, height: 50)
+                            .background(Color("Color"))
+                            .cornerRadius(4)
+                    })
                 }
                 .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : Color("deepRed"),lineWidth: 2))
-                .padding(25)
-
-                HStack {
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        
-                        self.reset()
-                        
-                    }) {
-                        
-                        Text("Forget password")
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("Color"))
-                    }
-                }
-                .padding(.top, 20)
                 
-                Button(action: {
-                    
-                    self.verify()
-                    
-                }) {
-                    
-                    Text("Log in")
-                        .foregroundColor(.white)
-                        .padding(.vertical)
-                        .frame(width: UIScreen.main.bounds.width - 50)
-                }
-                .background(Color("Color"))
-                .cornerRadius(10)
-                .padding(.top, 25)
-                
+                Spacer()
             }
-            }
-            
-        }
-
-    func verify(){
-    
-        if self.netID != "" && self.password != ""{
-            
-            Auth.auth().signIn(withEmail: self.netID, password: self.password) { (res, err) in
-                
-                if err != nil{
-                    
-                    self.error = err!.localizedDescription
-                    self.alert.toggle()
-                    return
-                }
-                
-                print("success")
-                UserDefaults.standard.set(true, forKey: "status")
-                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
-            }
-        }
-        else{
-            
-            self.error = "Please fill all the contents properly"
-            self.alert.toggle()
-        }
-    }
-
-    func reset(){
-        
-        if self.netID != ""{
-            
-            Auth.auth().sendPasswordReset(withEmail: self.netID) { (err) in
-                
-                if err != nil{
-                    
-                    self.error = err!.localizedDescription
-                    self.alert.toggle()
-                    return
-                }
-                
-                self.error = "RESET"
-                self.alert.toggle()
-            }
-        }
-        else{
-            
-            self.error = "Email Id is empty"
-            self.alert.toggle()
         }
     }
 }
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            Group {
-                ContentView()
-                    
-            }
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
+
+
