bc39adff91fc0e15135f1ce0b6ceaef5da80f2b1
comscience17 <hojunson3@gmail.com>
2022-02-25 20:18:57 -0500

    Digid logo on main page

--- digid/LandingView.swift
+++ digid/LandingView.swift
@@ -13,7 +13,10 @@ struct LandingView: View {
     @EnvironmentObject var appState: AppState
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("logo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 250, height: 150, alignment: .center)
         Button("Logout") {
             appState.hasOnboarded = false
             signOut()
