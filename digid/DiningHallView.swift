d865da7a4d7fc803661c641a59a92ec9992abb83
comscience17 <hojunson3@gmail.com>
2022-03-19 16:01:02 -0400

    commit

---
+++ digid/DiningHallView.swift
@@ -0,0 +1,20 @@
+//
+//  DiningHallView.swift
+//  digid
+//
+//  Created by Hojun Son on 2022/03/19.
+//
+
+import SwiftUI
+
+struct DiningHallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiningHallView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiningHallView()
+    }
+}
--- digid/LocationView.swift
+++ digid/LocationView.swift
@@ -36,31 +36,7 @@ struct LocationView: View {
     }
 }
 
-struct DiningHallView: View {
-    
-    @State var isNavigationBarHidden: Bool = true
-    
-    @State var buschMax: Int = 250
-    @State var liviMax: Int = 250
-    @State var browerMax: Int = 200
-    @State var neilsonMax: Int = 300
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Busch Dining Hall")
-                Text("Livingston Dining Hall")
-                Text("Brower Commons")
-                Text("Neilson Dining Hall")
-            }
-        }
-        .navigationBarTitle("Hidden Title")
-        .navigationBarHidden(self.isNavigationBarHidden)
-        .onAppear {
-            self.isNavigationBarHidden = true
-        }
-    }
-}
+
 
 struct GymView: View {
     
