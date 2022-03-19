56a04c738bffa75a962c41e1fd11b1e4d13a3d3d
comscience17 <hojunson3@gmail.com>
2022-03-19 16:01:42 -0400

    commit

--- digid/DiningHallView.swift
+++ digid/DiningHallView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct DiningHallView: View {
+    
+    @State var isNavigationBarHidden: Bool = true
+    
+    @State var buschMax: Int = 250
+    @State var liviMax: Int = 250
+    @State var browerMax: Int = 200
+    @State var neilsonMax: Int = 300
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Busch Dining Hall")
+                Text("Livingston Dining Hall")
+                Text("Brower Commons")
+                Text("Neilson Dining Hall")
+            }
+        }
+        .navigationBarTitle("Hidden Title")
+        .navigationBarHidden(self.isNavigationBarHidden)
+        .onAppear {
+            self.isNavigationBarHidden = true
+        }
     }
 }
 
