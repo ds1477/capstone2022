d1cf3f7a86ee7c26361d5fe33dbec2c66d73be62
comscience17 <hojunson3@gmail.com>
2022-03-19 18:08:47 -0400

    Commit

--- digid/GymView.swift
+++ digid/GymView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct GymView: View {
     
     @State var werblinFitnessMax: Int = 50
+    @State var werblinFitnessCurrent: Int = 50
     @State var liviFitnessMax: Int = 30
+    @State var liviFitnessCurrent: Int = 30
     @State var collegeAveFitnessMax: Int = 40
-    @State var cookDougFitnessMax: Int = 30
+    @State var collegeAveFitnessCurrent: Int = 40
+    @State var cookDougFitnessMax: Int = 40
+    @State var cookDougFitnessCurrent: Int = 35
     
     var body: some View {
         NavigationView {
