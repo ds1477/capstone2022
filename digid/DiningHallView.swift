13fa1e47065321f9da6465541fc17b280ca7cfa9
comscience17 <hojunson3@gmail.com>
2022-04-03 22:16:48 -0400

    Commit

--- digid/DiningHallView.swift
+++ digid/DiningHallView.swift
@@ -17,6 +17,10 @@ struct DiningHallView: View {
     @State var browerCurrent: Int = 160
     @State var neilsonMax: Int = 300
     @State var neilsonCurrent: Int = 270
+    @State var buschAvg: Int = 90
+    @State var liviAvg: Int = 104
+    @State var browerAvg: Int = 80
+    @State var neilsonAvg: Int = 90
     
     var body: some View {
         NavigationView {
@@ -26,24 +30,28 @@ struct DiningHallView: View {
                 VStack {
                     Text("Recommended Maximum Capacity: \(buschMax)")
                     Text("Current Capacity: \(buschCurrent)")
+                    Text("\(buschAvg)% filled")
                 }
                 Text("Livingston Dining Hall")
                 .fontWeight(.black)
                 VStack {
                     Text("Recommended Maximum Capacity: \(liviMax)")
                     Text("Current Capacity: \(liviCurrent)")
+                    Text("\(liviAvg)% filled")
                 }
                 Text("Brower Commons")
                 .fontWeight(.black)
                 VStack {
                     Text("Recommended Maximum Capacity: \(browerMax)")
                     Text("Current Capacity: \(browerCurrent)")
+                    Text("\(browerAvg)% filled")
                 }
                 Text("Neilson Dining Hall")
                 .fontWeight(.black)
                 VStack {
                     Text("Recommended Maximum Capacity: \(neilsonMax)")
                     Text("Current Capacity: \(neilsonCurrent)")
+                    Text("\(neilsonAvg)% filled")
                 }
             }
         }
