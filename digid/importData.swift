305552a2bc6b68f7d52bb94c626115027df30897
comscience17 <hojunson3@gmail.com>
2022-04-16 04:01:25 -0400

    Commit

---
+++ digid/importData.swift
@@ -0,0 +1,21 @@
+//
+//  importData.swift
+//  digid
+//
+//  Created by Hojun Son on 2022/04/16.
+//
+
+/*
+import Foundation
+import FirebaseDatabase
+
+class CapacityData {
+    let data = Database.database()
+    data.collection("sensor").addSnapshotListener {(snap, err) in
+        if err != nil {
+            print("An error has occurred.")
+            return
+        }
+    }
+}
+*/
