5c4a35c4f67786f241d68b87ca0353669e693a16
comscience17 <hojunson3@gmail.com>
2022-04-16 17:26:42 -0400

    Commit

--- digid/DataView.swift
+++ digid/DataView.swift
@@ -14,22 +14,20 @@ struct DataView: View {
         func initrealtimeFirebase() {
             FirebaseApp.configure()
         }
+        func importData() {
+            var ref = Database.database().reference()
+            ref.child("sensor").child("current_cap").getData(completion: {err, snapshot in
+                guard err == nil else {
+                    print("An error has occurred.")
+                    return;
+                }
+                let current_capacity = snapshot.value as? Int ?? 0;
+                print("Current Capacity: \(current_capacity)")
+            })
+        }
     }
 }
 
-
-
-func importData() {
-    var ref = Database.database().reference()
-    ref.child("sensor").child("current_cap").getData(completion: {err, snapshot in
-        guard err == nil else {
-            print("An error has occurred.")
-            return;
-        }
-        let current_capacity = snapshot.value as? Int ?? 0;
-        print("Current Capacity: \(current_capacity)")
-    })
-}
 
 struct DataView_Previews: PreviewProvider {
     static var previews: some View {
