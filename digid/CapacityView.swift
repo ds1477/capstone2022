e24f9473c4b59955f35868dfd79ac0cc3d4bd04d
comscience17 <hojunson3@gmail.com>
2022-04-16 15:59:03 -0400

    Commit

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -1,13 +1,8 @@
 //
-//  importData.swift
+//  CapacityView.swift
 //  digid
 //
 //  Created by Hojun Son on 2022/04/16.
 //
 
 import Foundation
-import FirebaseAuth
-
-class CapacityData {
-    
-}
