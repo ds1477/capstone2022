2143f421bac5f5dbaad4ab1404034479bef744a7
comscience17 <hojunson3@gmail.com>
2022-04-16 17:23:34 -0400

    C

--- digid/DataView.swift
+++ digid/DataView.swift
@@ -1,14 +1,19 @@
 //
-//  CapacityView.swift
+//  DataView.swift
 //  digid
 //
 //  Created by Hojun Son on 2022/04/16.
 //
 
-/*
 import SwiftUI
 import Foundation
 import Firebase
+
+struct DataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
 
 func initrealtimeFirebase() {
     FirebaseApp.configure()
@@ -25,4 +30,9 @@ func importData() {
         print("Current Capacity: \(current_capacity)")
     })
 }
-*/
+
+struct DataView_Previews: PreviewProvider {
+    static var previews: some View {
+        DataView()
+    }
+}
