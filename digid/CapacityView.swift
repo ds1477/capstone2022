88f2ec97c76f0208013bf7f41558940a59287910
comscience17 <hojunson3@gmail.com>
2022-04-16 16:47:19 -0400

    CommitC

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -9,7 +9,6 @@ import Foundation
 import FirebaseDatabase
 
 func importData() {
-    var browerCurrent: Int
     var ref = Database.database().reference()
     ref.child("sensor").child("current_cap").getData(completion: {err, snapshot in
         guard err == nil else {
