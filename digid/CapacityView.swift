b8ef4238b15d8703c946e45b6845744e1413111a
comscience17 <hojunson3@gmail.com>
2022-04-16 20:22:32 -0400

    Commit

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -6,6 +6,25 @@
 //
 
 import SwiftUI
+import Foundation
+import Firebase
+
+func initrealtimeFirebase() {
+    FirebaseApp.configure()
+}
+
+func importData() {
+    var ref = Database.database().reference()
+    ref.child("sensor").child("current_cap").getData(completion: {err, snapshot in
+        guard err == nil else {
+            print("An error has occurred.")
+            return;
+        }
+        let current_capacity = snapshot.value as? Int ?? 0;
+        print("Current Capacity: \(current_capacity)")
+    })
+}
+
 
 struct CapacityView: View {
     var body: some View {
