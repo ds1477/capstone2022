7b2d2749257f537980c8f779c2989b887ad7205b
comscience17 <hojunson3@gmail.com>
2022-04-19 14:03:57 -0400

    Comm

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Foundation
 import Firebase
 
-func importCurrent() -> Int {
+func importCurrent() {
     var ref = Database.database().reference()
     ref.child("sensor").child("current_cap").getData(completion: {err, snapshot in
         guard err == nil else {
