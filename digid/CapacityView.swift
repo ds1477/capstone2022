873f2955c60030c0e8fefe95cfe1e0f8451e3704
comscience17 <hojunson3@gmail.com>
2022-04-19 21:29:40 -0400

    C

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -49,10 +49,10 @@ struct CapacityView: View {
                 Spacer()
                 //let current_cap = importCurrent()
                 //let max_cap = importMax()
-                let average = 100 * (current_cap / max_cap)
+                //let average = 100 * (current_cap / max_cap)
                 Text("Current Capacity: \(current_cap)")
                 Text("Maximum Capacity: \(max_cap)")
-                Text("Capacity Filled: \(average)%")
+                //Text("Capacity Filled: \(average)%")
                 Spacer()
             }
         }
