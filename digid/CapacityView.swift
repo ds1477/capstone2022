ff816fa196c2796e02015a9ffc81367734870ed4
comscience17 <hojunson3@gmail.com>
2022-04-19 21:20:52 -0400

    Commit

--- digid/CapacityView.swift
+++ digid/CapacityView.swift
@@ -45,10 +45,12 @@ struct CapacityView: View {
         NavigationView {
             VStack {
                 Spacer()
-                let current_cap = importCurrent()
-                let max_cap = importMax()
-                Text("Current Capacity: \(current_cap)")
-                Text("Maximum Capacity: \(max_cap)")
+                Button {
+                    importCurrent()
+                }
+                Button {
+                    importMax()
+                }
                 Spacer()
             }
         }
