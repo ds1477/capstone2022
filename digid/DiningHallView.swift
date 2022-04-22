3a2bdda3398d52a4c8a32472bd66150571c45df4
comscience17 <hojunson3@gmail.com>
2022-04-21 22:06:50 -0400

    Commit

--- digid/DiningHallView.swift
+++ digid/DiningHallView.swift
@@ -50,6 +50,7 @@ struct DiningHallView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Image("dininghallpic")
                 VStack {
                     let current_cap = importCurrent()
                     let max_cap = importMax()
