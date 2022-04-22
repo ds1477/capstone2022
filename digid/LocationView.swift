d9a2560b9838d988638cd39d52c9d001651a8abe
comscience17 <hojunson3@gmail.com>
2022-04-21 22:05:03 -0400

    cCommit

--- digid/LocationView.swift
+++ digid/LocationView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 struct LocationView: View {
     var body: some View {
         NavigationView {
-            
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 150, alignment: .center)
+                Spacer()
+                Image("dininghallpic")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 150, alignment: .center)
+                NavigationLink(destination: DiningHallView(), label: {
+                    Text("Dining Hall Capacitance")
+                    .foregroundColor(Color.white)
+                    .frame(width:200, height: 50)
+                    .background(Color("Color"))
+                    .cornerRadius(4)
+                })
+                NavigationLink(destination: GymView(), label: {
+                    Text("Gym Capacitance")
+                    .foregroundColor(Color.white)
+                    .frame(width:200, height: 50)
+                    .background(Color("Color"))
+                    .cornerRadius(4)
+                })
+                Spacer()
+            }
         }
     }
 }
